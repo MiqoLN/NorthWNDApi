ac56c502cb41ad3da476ca42b1de6c094d394275
Michael Shirinyan <mshirinyan@outlook.com>
2021-03-07 20:19:16 +0400

    Modified OrdersController

---
+++ NorthWNDApi/Models/DB/GetRandView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NorthWNDApi.Models.DB
+{
+    public partial class GetRandView
+    {
+        public double? Value { get; set; }
+    }
+}
---
+++ NorthWNDApi/Models/DB/ProductsAboveAveragePrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NorthWNDApi.Models.DB
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+}
